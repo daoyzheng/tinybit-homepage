f07a32bf5368a8f6d1c5751229163d39827abf94
Dao Zheng <daoyzheng@gmail.com>
2022-04-20 20:40:45 -0600

    Add layout and theme component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import Theme from '../components/Theme'
+import { THEME } from '../lib/constants'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState(THEME.dark)
+  return (
+    <Theme theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Theme>
+  )
 }
 
 export default MyApp
