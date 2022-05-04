f33414cb33fe0e39ae9ebc1125b683e523734e91
Dao Zheng <daoyzheng@gmail.com>
2022-05-03 19:07:17 -0600

    Add footer styling

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="mb-5 text-center text-xs text-slate-400">
       <a href="https://github.com/daoyzheng/tinybit-homepage" target="_blank" rel="noreferrer">
-        <div>Designed and built by Dao Yuan Zheng</div>
+        <div className="text-orange-300 underline">Designed and built by Dao Yuan Zheng</div>
       </a>
       <div>© 2022 Dao Yuan Zheng. All rights reserved</div>
     </div>
